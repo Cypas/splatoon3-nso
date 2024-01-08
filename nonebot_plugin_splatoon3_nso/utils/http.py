26ea5c2ca64eb6e96804e2b802acd05669642497
cypas <ayano05@outlook.com>
2024-01-08 14:38:07 +0800

    完善工具函数

---
+++ nonebot_plugin_splatoon3_nso/utils/http.py
@@ -0,0 +1,105 @@
+import cfscrape
+import httpx
+from httpx import Response
+
+from ..config import plugin_config
+
+proxy_address = plugin_config.splatoon3_proxy_address
+
+global_client_dict: dict[httpx.Client] = {}  # 登录涉及函数login in和login_2需要保持一段时间浏览器状态，在输入npf码完成登录后需要关闭client
+
+
+def cf_http_get(url: str):
+    """cf get"""
+    global proxy_address
+    # 实例化一个create_scraper对象
+    scraper = cfscrape.create_scraper()
+    # 请求报错，可以加上时延
+    # scraper = cfscrape.create_scraper(delay = 6)
+    if proxy_address:
+        proxies = {
+            "http": "http://{}".format(proxy_address),
+            "https": "http://{}".format(proxy_address),
+        }
+        # 获取网页内容 代理访问
+        res = scraper.get(url, proxies=proxies)
+    else:
+        # 获取网页内容
+        res = scraper.get(url)
+    return res
+
+
+async def async_http_get(url: str) -> Response:
+    """async http_get"""
+    if proxy_address:
+        proxies = "http://{}".format(proxy_address)
+        async with httpx.AsyncClient(proxies=proxies) as client:
+            response = await client.get(url, timeout=5.0)
+            return response
+    else:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, timeout=5.0)
+            return response
+
+
+def http_get(url: str) -> Response:
+    """http_get"""
+    if proxy_address:
+        proxies = "http://{}".format(proxy_address)
+        response = httpx.get(url, proxies=proxies, timeout=5.0)
+    else:
+        response = httpx.get(url, timeout=5.0)
+    return response
+
+
+async def get_file_url(url):
+    """从网页读获取图片"""
+    resp = await async_http_get(url)
+    resp.read()
+    data = resp.content
+    return data
+
+
+def get_or_init_login_client(msg_id):
+    """为每个登录会话创建唯一client，防止公共变量覆盖"""
+    global global_client_dict
+    client: httpx.Client = global_client_dict.get(msg_id)
+    if client:
+        return client
+    else:
+        client = httpx.Client()
+        global_client_dict.update({msg_id: client})
+        return client
+
+
+def close_login_client(msg_id):
+    """关闭login client"""
+    global global_client_dict
+    client: httpx.Client = global_client_dict.get(msg_id)
+    if client:
+        client.close()
+        global_client_dict.pop(msg_id)
+
+
+def close_all_login_client():
+    """关闭全部login client"""
+    global global_client_dict
+    for client in global_client_dict.values():
+        client.close()
+    global_client_dict.clear()
+
+
+class ClientReq(object):
+    """httpx 请求封装为异步client请求"""
+
+    @staticmethod
+    def get(*args, **kwargs):
+        async with httpx.AsyncClient() as client:
+            response = await client.get(*args, **kwargs)
+            return response
+
+    @staticmethod
+    def post(*args, **kwargs):
+        async with httpx.AsyncClient() as client:
+            response = await client.post(*args, **kwargs)
+            return response
---
+++ nonebot_plugin_splatoon3_nso/utils/time.py
@@ -0,0 +1,106 @@
+import datetime
+import re
+from datetime import timedelta
+
+time_format_ymdh = "%Y-%m-%dT%H"
+
+
+# 类 时区工具
+class TimeUtil(object):
+    @classmethod
+    def parse_timezone(cls, timezone):
+        """
+        解析时区表示
+        :param timezone: str eg: +8
+        :return: dict{symbol, offset}
+        """
+        result = re.match(r"(?P<symbol>[+-])(?P<offset>\d+)", timezone)
+        symbol = result.groupdict()["symbol"]
+        offset = int(result.groupdict()["offset"])
+
+        return {"symbol": symbol, "offset": offset}
+
+    @classmethod
+    def convert_timezone(cls, dt, timezone="+0") -> datetime.datetime:
+        """默认是utc时间，需要提供时区"""
+        result = cls.parse_timezone(timezone)
+        symbol = result["symbol"]
+
+        offset = result["offset"]
+
+        if symbol == "+":
+            return dt + timedelta(hours=offset)
+        elif symbol == "-":
+            return dt - timedelta(hours=offset)
+        else:
+            raise Exception("dont parse timezone format")
+
+
+def get_expire_time() -> str:
+    """计算过期时间 字符串 精确度为 ymdh"""
+    # 计算过期时间
+    time_now = get_time_now_china()
+    time_now_h = time_now.hour
+    # 计算过期时间字符串
+    # 判断当前小时是奇数还是偶数
+    expire_time: datetime
+    if (time_now_h % 2) == 0:
+        # 偶数
+        expire_time = time_now + datetime.timedelta(hours=2)
+    else:
+        expire_time = time_now + datetime.timedelta(hours=1)
+    expire_time_str = expire_time.strftime(time_format_ymdh).strip()
+    return expire_time_str
+
+
+def time_converter(time_str) -> datetime:
+    """时间转换 年-月-日 时:分:秒"""
+    # convert time to UTC+8
+    dt = datetime.datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%SZ")
+    dt += datetime.timedelta(hours=8)
+    return dt
+
+
+def time_converter_yd(time_str):
+    """时间转换 月-日"""
+    dt = time_converter(time_str)
+    return datetime.datetime.strftime(dt, "%m.%d")
+
+
+def time_converter_hm(time_str):
+    """时间转换 时:分"""
+    dt = time_converter(time_str)
+    return datetime.datetime.strftime(dt, "%H:%M")
+
+
+def time_converter_mdhm(time_str):
+    """时间转换 月-日 时:分"""
+    dt = time_converter(time_str)
+    return datetime.datetime.strftime(dt, "%m-%d %H:%M")
+
+
+def time_converter_weekday(time_str):
+    """时间转换 周几，如周一"""
+    dt = time_converter(time_str)
+    weekday = dt.weekday()
+    return weekday
+
+
+def get_time_ymd():
+    """获取年月日"""
+    dt = get_time_now_china().strftime("%Y-%m-%d")
+    return dt
+
+
+def get_time_y() -> int:
+    """获取年"""
+    year = get_time_now_china().year
+    return year
+
+
+def get_time_now_china() -> datetime.datetime:
+    """获取中国所在东八区时间"""
+    # 获取utc时间，然后转东8区时间
+    utc_now = datetime.datetime.utcnow()
+    convert_now = TimeUtil.convert_timezone(utc_now, "+8")
+    return convert_now
