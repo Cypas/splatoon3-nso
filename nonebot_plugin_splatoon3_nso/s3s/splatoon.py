872b28db74698e4f16d3d093d3f0bd24577f7f3f
cypas <ayano05@outlook.com>
2024-03-28 12:21:28 +0800

    修改nso鱿鱼圈封禁时的提示文本

--- nonebot_plugin_splatoon3_nso/s3s/splatoon.py
+++ nonebot_plugin_splatoon3_nso/s3s/splatoon.py
@@ -131,7 +131,7 @@ class Splatoon:
                 if "has be banned" in e:
                     # 鱿鱼圈封禁
                     user = dict_get_or_set_user_info(self.platform, self.user_id, _type=self.dict_type, user_agreement=-1)
-                    msg = f"喷3账号 {user.game_name or ''} 鱿鱼圈被封禁，无法使用相关查询，一般会在2-3周后自动解封"
+                    msg = f"喷3账号 {user.game_name or ''} 鱿鱼圈被封禁，无法使用相关查询，一般会在4周，即28天后自动解封"
                     if self.bot and self.event:
                         # 来自用户主动请求
                         await bot_send(self.bot, self.event, msg)
