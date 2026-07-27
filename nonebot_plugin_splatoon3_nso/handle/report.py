dbe852ec88c2898f45104d3d110f58afccb34420
cypas <ayano05@outlook.com>
2026-07-27 15:50:12 +0800

    perf:减少每日日报处理用户

--- nonebot_plugin_splatoon3_nso/handle/report.py
+++ nonebot_plugin_splatoon3_nso/handle/report.py
@@ -79,7 +79,8 @@ async def report(bot: Bot, event: Event, re_tuple: Tuple = RegexGroup()):
             msg = f"没有查询到所指定日期的日报数据"
         else:
             msg = f"数据准备中，在登陆bot两天后才可获取日报对比数据"
-        msg += f'\n查看近30次日报: /report_all\n\n现在小鱿鱿用户量过多，为减少日报造成的请求负担，现在只有第一天使用过nso查询功能的用户，才会在第二天生成对应的日报数据'
+        msg += f'\n查看近30次日报: /report_all'
+        msg += f'\n\n现在小鱿鱿用户量过多，为减少日报造成的请求负担，现在只有第一天使用过nso查询功能的用户，才会在第二天生成对应的日报数据'
         await bot_mixed_send_report(bot, event, title="未获取到日报", msg=msg)
     else:
         # 有日报数据
@@ -190,7 +191,8 @@ def get_report(platform, user_id, report_day=None, _type="normal"):
         if old.coop_gold != new.coop_gold:
             str_coop += f' 🥉{new.coop_gold - old.coop_gold:+}'
         msg += f'鳞片: {str_coop}\n'
-    msg += f'查看近30次日报: /report_all\n\n现在小鱿鱿用户量过多，为减少日报造成的请求负担，现在只有第一天使用过nso查询功能的用户，才会在第二天生成对应的日报数据'
+    msg += f'查看近30次日报: /report_all'
+    msg += f'\n\n现在小鱿鱿用户量过多，为减少日报造成的请求负担，现在只有第一天使用过nso查询功能的用户，才会在第二天生成对应的日报数据'
     # u = get_user(user_id=user_id)
     # if report_day and fst_day and not u.report_type:
     #     msg += f'```\n\n订阅早报: /report```'
